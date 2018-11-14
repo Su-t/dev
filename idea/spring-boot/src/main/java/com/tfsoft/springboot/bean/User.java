96d62dae5ae2a629a0e83350036d0e273a8095e8
Su-t <786641785@qq.com>
2018-11-15 00:22:31 +0800

    使用spring -boot 自带的json框架让页面显示json数据

---
+++ idea/spring-boot/src/main/java/com/tfsoft/springboot/bean/User.java
@@ -0,0 +1,30 @@
+/**
+ * tfsoft.com Inc.
+ * Copyright (c) 2009-2018 All Rights Reserved.
+ */
+package com.tfsoft.springboot.bean;
+
+/**
+ * 这是一个测试实体类
+ * @author st st@tfsoft.com on 2018-11-14
+ */
+public class User {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- idea/spring-boot/src/main/java/com/tfsoft/springboot/controller/HelloSpringboot.java
+++ idea/spring-boot/src/main/java/com/tfsoft/springboot/controller/HelloSpringboot.java
@@ -4,6 +4,7 @@
  */
 package com.tfsoft.springboot.controller;
 
+import com.tfsoft.springboot.bean.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,11 +16,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloSpringboot {
 
     /**
-     *
+     * 1 在页面上显示hello。
      * @return
      */
     @RequestMapping("/helloSpringboot")
     public String helloSpringboot(){
         return "hello";
+    }
+
+    /**
+     * 2 在页面上显示返回的json数据（因为spring boot默认使用的json解析框架是jackson）
+     * @return
+     */
+    @RequestMapping("/getUser")
+    public User getUser(){
+        User user = new User();
+        user.setId(1);
+        user.setName("苏腾");
+        return user;
     }
 }
